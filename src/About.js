4aa01162f3fe3baaca499259d6d1cb9254907a3e
adianbu <57458067+adianbu@users.noreply.github.com>
2021-05-20 23:06:36 +0530

    Changed about color and added a few animations

--- src/About.js
+++ src/About.js
@@ -3,18 +3,31 @@ import "./About.css";
 import {Avatar} from "@material-ui/core";
 import dp from './images/dp.jpg';
 
-
+import ScrollAnimation  from 'react-animate-on-scroll'
+import Fade from 'react-reveal/Fade';
 
 
 function About() {
     return (
+//         <ScrollAnimation animateIn="animate__backInUp" duration="0.5" 
+//         style={{    transition: "all 1s cubic-bezier(0.075, 0.82, 0.165, 1)"
+// }}>
         <div className="about" id="about">
-            <p className="about_title">About</p>
+        
+        <p className="about_title">
+        <Fade up>About</Fade></p>
+        
+           
 
             <div className="about_center">
 
+            <Fade left cascade>
+
             <div className="about_left">
-              <p className="about_left_title">Skill sets</p>
+                
+                <p className="about_left_title">Skill sets</p>
+                
+              
                 <div className="about_skillsets">      
                     <p className="about_skillsets_left">
                         Javascript<br/>
@@ -31,8 +44,10 @@ function About() {
                         
                     </p>              
                 </div>
+                
 
             </div>
+            </Fade>
 
             <div className="about_right">
                 <div className="about_avatar_div">
@@ -45,7 +60,7 @@ function About() {
                     <p>
                     Hi, I'm Adithya Anbu. I am a full stack WEB and APP developer. 
                     I have worked on quite a few projects and I would love to work on yours. 
-                    For more information you can <span> <a href="#contact" style={{textDecoration:"none",color:"#e6276d" }}>contact me </a></span>here. 
+                    For more information you can <span> <a id="about_right_link" href="#contact" style={{textDecoration:"none" }}>contact me </a></span>here. 
                     </p>
                 </div>
             </div>
@@ -57,6 +72,7 @@ function About() {
             
            
         </div>
+        // </ScrollAnimation>
     )
 }
 
