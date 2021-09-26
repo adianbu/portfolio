92d2ea03ac22229def5970e1e559dbc7953bdc68
adianbu <57458067+adianbu@users.noreply.github.com>
2021-09-26 20:01:12 +0530

    random

--- src/Random.js
+++ src/Random.js
@@ -20,6 +20,32 @@ function Random() {
             Front camera features - Face unlock, HDR, screen flash, face retouching, filters, ultra wide selfie
             Box also includes: Screen protector (pre-applied), Warp Charge 30 power adapter , Warp Type-C cable (Support USB 2.0), Quick start guide, Welcome letter, Safety information and warranty card, Brand sticker, Phone case, SIM tray ejector, Highest SAR value & Plastic recycling card, Red Cable Club Membership card supports Alexa Hands-Free. Alexa on your phone lets you make phone calls, open apps, control smart home devices, access the library of Alexa skills, and more using just your voice while on-the-go. Download the Alexa app and complete hands-free setup to get started. Just ask - and Alexa will respond instantly.
             </p>
+
+            <p>
+            OnePlus Technology (Shenzhen) Co., Ltd. (一加科技) is a Chinese consumer electronics manufacturer headquartered in Shenzhen, Guangdong province, in the Tairan Building (泰然大厦) at the Chegong Temple (车公庙) subdistrict of Futian District.[6] It was founded by Pete Lau and Carl Pei in December 2013, and is currently majority-owned by Oppo as its only shareholder, which is a subsidiary of BBK Electronics along with Vivo, Realme and iQOO. The company is best known for making smartphones, and officially serves 34 countries and regions around the world as of July 2018.
+            nePlus was founded on 17 December 2013 by former Oppo vice-president Pete Lau and Carl Pei.[7] According to Chinese public records, OnePlus' only institutional shareholder is Oppo Electronics.[4] Lau denied that OnePlus was a wholly owned subsidiary of Oppo and stated that Oppo Electronics and not Oppo Mobile (the phone manufacturer) is a major investor of OnePlus and that they are "in talks with other investors",[8] although OnePlus has confirmed it uses Oppo's manufacturing line and shares part of the supply chain resources with Oppo.[9] The company's main goal was to design a smartphone that would balance high-end quality with a lower price than other phones in its class, believing that users would "Never Settle" for the lower-quality devices produced by other companies. Lau explained that "we will never be different just for the sake of being different. Everything done has to improve the actual user experience in day-to-day use."[10][11] He also showed aspirations of being the "Muji of the tech industry", emphasizing its focus on high-quality products with simplistic, user-friendly designs.[10] Continuing Lau's association with the platform from the Oppo N1,[11] OnePlus entered into an exclusive licensing agreement with Cyanogen Inc. to base its products' Android distribution upon a variant of the popular custom ROM CyanogenMod and use its trademarks outside of China.[12][13]
+
+The company unveiled its first device, the OnePlus One, on 23 April 2014 which was intended to capture market from the Google Nexus series.[10] In December 2014, alongside the release of the OnePlus One in India exclusively through Amazon, OnePlus also announced plans to establish a presence in the country, with plans to open 25 official walk-in service centers across India.[14]
+
+In April 2014, OnePlus hired Han Han to help market its products in mainland China.
+
+On 9 March 2014, the company expanded its operations to the European Union.
+
+On 16 December 2014, The Supreme Court of India and Delhi High Court banned the import and sale of OnePlus One phones following a lawsuit by Micromax alleging it has exclusivity for shipping phones with Cyanogen OS software in India.[17] On 21 December 2014, the ban was lifted. The device continues to be shipped with Cyanogen OS; however, a customized version of Android specially designed by OnePlus and named OxygenOS has been released, allowing later OnePlus devices to be sold in India.[18][19]
+
+OnePlus made its products available in Southeast Asia for the first time, partnering with Lazada Indonesia[20] on 23 January 2015 and was expected to expand during that year throughout the region. In June 2016, OnePlus decided to pull out of the Indonesian market due to local regulations for imported 4G smartphones restricting sales of the OnePlus 2.[21]
+
+At the launch event of the OnePlus 6 on 17 May 2018, OnePlus announced it would be opening five new OnePlus Experience stores in India, as well as 10 new service centers.[22]
+
+According to market research firm Counterpoint Research, OnePlus topped the Indian premium smartphone market last year with a 33 percent share, beating Samsung Electronics with 26 percent. In the IDC survey, OnePlus ranked third in India's market with more than $500 in the first quarter of this year after Apple and Samsung Electronics. And it ranked second after China's Vivo in the 300-500-dollar market.[23]
+
+On 16 October 2020, Carl Pei resigned as director of OnePlus.[24][25]
+
+In 2021 Oppo and OnePlus combined their hardware research teams via OPlus.[26]
+
+In July 2021 OnePlus Merges OxygenOS With Oppo's ColorOS, Both companies’ software will remain separate and continue to serve their individual regions (OxygenOS for OnePlus phones globally, ColorOS on OnePlus and Oppo devices in China) but share a common codebase, which OnePlus says should standardize its software experience and streamline the development process for future OxygenOS updates
+
+            </p>
         </div>
     )
 }
